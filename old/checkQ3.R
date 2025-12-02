03aa3cace6ac54077e98b28b38eec3fee6889c2f
Lenin Rafael Riera Segura <leninrafael.rierasegura@kaust.edu.sa>
2025-12-02 18:46:36 +0300

    update, now everything almost ok, just order to compute the error is missing

--- old/checkQ3.R
+++ old/checkQ3.R
@@ -11,7 +11,7 @@ source(here::here("all_functions.R"))
 source(here::here("matern_functions.R"))
 
 # parameters
-h <- 0.02
+h <- 0.2
 kappa <- 5
 sigma <- 0.8
 alpha <- 1.4
@@ -21,7 +21,7 @@ tau <- sqrt(gamma(nu) / (sigma^2 * kappa^(2*nu) * (4*pi)^(1/2) * gamma(nu + 1/2)
 n.overkill <- 1000
 
 # build a graph with a mesh
-graph_initial <- gets.graph.tadpole(flip_edge = FALSE)
+graph_initial <- gets.graph.tadpole(flip_edge = TRUE)
 graph <- graph_initial$clone()
 
 graph_initial$build_mesh(h = h)
@@ -32,7 +32,7 @@ graph$add_observations(
   data = graph_initial$get_mesh_locations() %>% 
     as.data.frame() %>% 
     mutate(y = 1) %>% 
-    rename(edge_number = V1, distance_on_edge = V2),
+    rename(edge_number = V1, distance_on_edge = V2)%>%mutate(dummy=1:length(y)),
   edge_number = "edge_number",
   distance_on_edge = "distance_on_edge",
   data_coords = "PtE",
@@ -49,6 +49,11 @@ Approx_Sigma <- gets_cov_mat_rat_approx_alpha_1_to_2(
   alpha = alpha, 
   m = m)
 
+
+
+order_tmp <- graph$get_data()$dummy
+Approx_Sigma[order_tmp,] <- Approx_Sigma[1:nrow(Approx_Sigma),]
+
 graph_true <- gets.graph.tadpole(flip_edge = FALSE)
 graph_true$build_mesh(h = h)
 graph_true$compute_fem()
@@ -59,7 +64,12 @@ True_Sigma <- gets_true_cov_mat(graph = graph_true,
                                 n.overkill = n.overkill)
 
 q <- graph_true$plot_function(X = True_Sigma[,2], type = "plotly", line_color = "red", interpolate_plot = FALSE, name = "True", showlegend = TRUE)
-graph_initial$plot_function(X = Approx_Sigma[,2], p = q, type = "plotly", line_color = "blue", interpolate_plot = FALSE, name = "Approx", showlegend = TRUE)
+
+graph_initial$plot_function(X = Approx_Sigma[,1], p = q, type = "plotly", line_color = "blue", interpolate_plot = FALSE, name = "Approx", showlegend = TRUE)
+
+
+Approx_Sigma[order_tmp, ] <- Approx_Sigma[,order_tmp]
+
 
 L_2_error = sqrt(as.double(t(graph_true$mesh$weights)%*%(True_Sigma - Approx_Sigma)^2%*%graph_true$mesh$weights))
 print(L_2_error)
